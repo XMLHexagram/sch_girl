3135a960eef5bdc1befefbef70c7e15a9ad897b6
Hexagram <lmx_Hexagram@outlook.com>
2020-08-28 17:50:56 +0800

    import material ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 function App() {
-  return <div>Hello React , Webpack And Electron</div>
+  return (
+    <div>
+      <span>Hello React , Webpack And Electron</span>
+      <Button variant="contained" color="primary">
+        Hello Material UI
+      </Button>
+    </div>
+  )
 }
 
 export default App
